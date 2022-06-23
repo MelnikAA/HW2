d687a432b63d6346e947f9211ed59c5c8a808cea
MelnikAA <i89141800166@yandex.ru>
2022-06-23 11:18:49 +1000

    fixed#1

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 import motorists from '/motorists.json' assert {type:'json'};
 console.log(motorists);
 
-for (var i = 0; i < motorists.length; i++) {
-    var tr;
-    tr = $('<tr/>');
+for (let i = 0; i < motorists.length; i++) {
+    const tr = $('<tr/>');
     tr.append("<td>" + motorists[i].person.firstname + " " + motorists[i].person.lastname + "</td>");
     tr.append("<td>" + motorists[i].car.manufacturer + "</td>");
     tr.append("<td>" + motorists[i].car.model + "</td>");
